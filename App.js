98d95db0e23e0957a0a79be06857160a863fe059
KevinJeanMarie <kjeanmarie01@gmail.com>
2022-01-20 00:40:09 +0100

    profil page 85% finish, homepage added on routes and 404 NOT FOUND PAGE

---
+++ App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { UserContextProvider } from './contexts/User'
+
+import CreateUser from "./Pages/CreateUser";
+import Profil from "./Pages/Profil";
+import Profile from "./Pages/Profile";
+import CreateTweetForm from "./components/Tweet/CreateTweetForm";
+import NotFound from "./Pages/NotFound";
+import Home from "./Pages/Home";
+
+
+const App = () => {
+  return (
+  <BrowserRouter>
+    <UserContextProvider>
+      <Routes>
+        <Route exact path='/' element={<CreateUser />} />
+        <Route exact path='/profil' element={<Profile />} />
+        <Route exact path='/home' element={<Home />} />
+        <Route exact path='*' element={<NotFound/>}/>  
+      </Routes>
+    </UserContextProvider>
+  </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ components/Tweet/CreateTweetModal.js
@@ -0,0 +1,53 @@
+import Modal from 'react-modal'
+import { GrClose } from 'react-icons/gr';
+import styled from 'styled-components'
+import CreateTweetForm from './CreateTweetForm';
+
+
+const PictureTweet = styled.div`
+.picturetweetprofile img {
+border-radius: 50px;
+width: 50px;
+height: 50px;
+border: 1px solid rgb(212, 210, 210);
+margin-top: 30px;
+}
+`
+
+const modalStyle = {
+  content: {
+    top: '30%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    width: '40%',
+    height: '280px',
+    borderRadius: '20px'
+    }
+}
+
+
+const CreateTweetModal = ({ isOpen, onClose }) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      style={modalStyle}
+    > 
+        <GrClose onClick={onClose} />
+    
+    <PictureTweet>
+        <div className="picturetweetprofile">
+          <img src="https://ciyaaro.com/wp-content/uploads/2021/05/Chelsea-vs-Real-Madrid.jpg"/>
+          </div>
+          </PictureTweet>
+     
+      <CreateTweetForm
+        onClose={onClose}
+      />
+    </Modal>
+  )
+}
+
+export default CreateTweetModal;
